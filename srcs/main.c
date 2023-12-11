a5da997c21ffdd3c7f5b188057d6ba3b34d01316
irn66 <126900447+idumenil@users.noreply.github.com>
2023-12-11 10:51:02 +0100

    Add files via upload

--- srcs/main.c
+++ srcs/main.c
@@ -1,68 +1,123 @@
 #include "../includes/minishell.h"
 
-t_data g_data;
+void	prt_args(t_data *datas)
+{
+	int	i = -1;
+	while (datas->args_arr[++i])
+	{
+		printf("\nargs_arr[%d]\n >> ", i);
+		int j = -1;
+		while (datas->args_arr[i][++j])
+			printf("[%d]{%s} ", j, datas->args_arr[i][j]);
+		printf("[%d]NULL ", j);
+	}
+	printf("\nargs_arr[%d] >> NULL", i);
+	printf("\n");
+}
 
-void	prt_args(char **args)
+int	cmd_launcher(t_data *datas)
 {
 	int	i;
+	int		err;
 
-	i = 0;
-	printf("args: ");
-	while (args[i])
+	i = -1;
+	while (datas->args_arr[++i])
 	{
-		printf("%d[%s] ", i, args[i]);
-
-		i++;
+		if (datas->args_arr[i][0][0] != '|')
+		{
+			printf("\n>>> Command output:\n");
+			err = look_for_builtin(datas->args_arr[i], datas);	// builtins
+			if (err != 0)
+				err = my_execve(datas->args_arr[i], datas);		// system program
+			if (err)
+				printf("%s: command not found\n", datas->args_arr[i][0]);
+		}
 	}
-	printf("\n\n");
+	return (err);
 }
 
+//char ***args_arr[0] = {"echo", "La maison", NULL};
+//char ***args_arr[1] = {"|", NULL};
+//char ***args_arr[2] = {"echo", "Coucou", NULL};
+//char ***args_arr[3] = {"|", NULL};
+//char ***args_arr[4] = {"grep", "o", ">", "file" NULL};
+//char ***args_arr[4] = NULL
 
-void	main_command_loop(void)
+void	main_command_loop(t_data *datas)
 {
 	int		err;
-	char	**args;
 
 	(void)err;
-	while (!g_data.exit)
+	while (!datas->exit)
 	{
 		err = 0;
-		g_data.cmd_line = readline(g_data.prompt);
+		make_prompt(datas);
+		datas->cmd_line = readline(datas->prompt);	// lecture ligne de commande
 
-		if (g_data.cmd_line == NULL)
+		if (datas->cmd_line == NULL)				// gestion CTRL D > STOP LOOP
 			break ;
-		if (g_data.cmd_line[0])
+		if (datas->cmd_line[0])						// si la ligne n'est pas vide
 		{
-			add_history(g_data.cmd_line);
+			add_history(datas->cmd_line);			// ajoute la ligne a l'historique
 
-			args = parse(g_data.cmd_line);
-			prt_args(args);
-
-			err = look_for_builtin(args);	// builtins
-			if (err != 0)
-				err = my_execve(args);	// system program
+			err = parse(datas->cmd_line, datas);	// parsing (err si les quotes ne ferment pas)
+			if (!err)
+			{
+				prt_args(datas);
+				err = cmd_launcher(datas);			// lance les commandes
+				clear_data_args_arr(datas);			// free ***data.args_arr
+			}
 		}
-		free(g_data.cmd_line);
+		free(datas->cmd_line);
 	}
 }
-
-//char *args[] = {"echo", "La maison", NULL};
 
 int	main(int argc, char **argv, char **env)
 {
+	t_data	datas;
+
 	(void)argc;
 	(void)argv;
 
-	// int		found;
-	// char	**args;
+	signal(SIGQUIT, SIG_IGN);
+	signal(SIGINT, handlerctrlc);
 
-	init_vars(env);
-	// copy_env_var();
-	// prt_args(g_data.copy_env);
-	main_command_loop();
-	printf("start my_export");
-	exit_minishell();
+	init_vars(env, &datas);
 
-	// exit 42 ? (y)19
+	main_command_loop(&datas);
+	// parse("echo asdfa dF\"ads f\"sadf sdf's df's\"0\"\"1\" \"2\"dfg'gdsfgdf'g\" ' dfg'   dfgdfg\"\"yui\"'ohb'ghjk'o'     d\"\"\"gdf \"'g'h|", &datas);
+	// prt_args(&datas);
+	// clear_data_args_arr(&datas);
+
+	exit_minishell(&datas);
+
 	return (0);
 }
+
+/*
+
+// Fonction de gestionnaire de signal pour SIGINT (Ctrl-C)
+void sigint_handler(int signo) {
+    printf("\nVous avez appuyé sur Ctrl-C. Affichage du prompt : ");
+    fflush(stdout);  // Assure que le message est affiché immédiatement
+
+    // Ajoutez ici votre logique de traitement ou votre prompt personnalisé
+
+    // Réinitialise le gestionnaire de signal pour SIGINT
+    signal(SIGINT, sigint_handler);
+}
+
+int main()
+{
+    // Définit le gestionnaire de signal pour SIGINT
+    signal(SIGINT, sigint_handler);
+
+    // Votre programme principal ici
+    while (1) {
+        // Effectuez vos opérations principales ici
+    }
+
+    return 0;
+}
+
+*/
